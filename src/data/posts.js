7b6d302213639bce16e17fd01657f241155a3dfe
Ana Sequeira <anacseq@gmail.com>
2020-07-16 23:02:02 +0100

    data posts

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,5 +4,17 @@ export const posts = [
       description: "CSS (Cascading Style Sheets) is a style sheet language that describes the presentation of web pages. Whereas HTML is what forms the structure of a web page, CSS is what we use to style the HTML with colors, backgrounds, font sizes, layout, and more. As you'll soon learn, CSS is one of the core technologies for designing and building websites.",
       img_src:"https://achievement-images.teamtreehouse.com/badges_css_basics_stage6.png",
       id: "css-1"
-    }
+    },
+    {
+        title: "CSS Basics",
+        description: "CSS (Cascading Style Sheets) is a style sheet language that describes the presentation of web pages. Whereas HTML is what forms the structure of a web page, CSS is what we use to style the HTML with colors, backgrounds, font sizes, layout, and more. As you'll soon learn, CSS is one of the core technologies for designing and building websites.",
+        img_src:"https://achievement-images.teamtreehouse.com/badges_css_basics_stage6.png",
+        id: "css-1"
+      },
+      {
+        title: "CSS Basics",
+        description: "CSS (Cascading Style Sheets) is a style sheet language that describes the presentation of web pages. Whereas HTML is what forms the structure of a web page, CSS is what we use to style the HTML with colors, backgrounds, font sizes, layout, and more. As you'll soon learn, CSS is one of the core technologies for designing and building websites.",
+        img_src:"https://achievement-images.teamtreehouse.com/badges_css_basics_stage6.png",
+        id: "css-1"
+      },
 ];
