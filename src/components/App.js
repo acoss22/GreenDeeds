31b37b1a75019930a0eab6a2335d6ccd6fe0095e
Ana Sequeira <anacseq@gmail.com>
2020-07-20 11:44:28 +0100

    lifecycle

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,39 @@ import React from "react";
 import "../styles/index.css";
 import Header from "./Header";
 
-
-import Home from './Home';
+import Home from "./Home";
 
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { lat: null};
+    this.state = { lat: null };
   }
- 
-  render() {
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {this.setState({ lat: position.coords.latitude }); 
-    },
-      err => console.log(err)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
     );
+    console.log("My component was rendered to the screen");
+    
+  }
+
+  componentDidUpdate(){
+    console.log("My component was just updated! ");
+  }
+
+  render() {
+  
 
     return (
       <div className="App">
         <Header />
-    
+
         <Home />
         <p> Latitude: {this.state.lat} </p>
+       
       </div>
     );
   }
