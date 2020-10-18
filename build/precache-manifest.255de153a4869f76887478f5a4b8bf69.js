6791745800b3f9984e6fe183bf30d14c283966ba
AnaCS <anacseq@gmail.com>
2020-10-18 14:41:29 +0100

    many changes

---
+++ build/precache-manifest.255de153a4869f76887478f5a4b8bf69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0572fb6353f5e3a0a58b0e1c9b8729d9",
+    "url": "/greendeeds/index.html"
+  },
+  {
+    "revision": "9c551e2dae736e364c21",
+    "url": "/greendeeds/static/css/main.1892beb6.chunk.css"
+  },
+  {
+    "revision": "e536f8bf8a18ea575872",
+    "url": "/greendeeds/static/js/2.d7bcf61d.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/greendeeds/static/js/2.d7bcf61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c551e2dae736e364c21",
+    "url": "/greendeeds/static/js/main.12206267.chunk.js"
+  },
+  {
+    "revision": "726e3438896362abd1f6",
+    "url": "/greendeeds/static/js/runtime-main.c030d7d2.js"
+  }
+]);
